e4447cb3515dcac567640847a358685e41532f69
Chen Leikehmacher <chen.leikehmacher@gmail.com>
2019-11-16 20:33:43 +0200

    readme + rename

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ When launched, client will begin performing periodic API requests to fetch /v/'s
 The largest post ID will be collected, and by comparing ID from previous request an estimate of
 getting a 5 is roughly calculated, assuming a post is submitted immediately after the percentage is displayed.
 
+Does it work? eh... relying on 4chan's API for this type of deduction is like shorting a stock from a bank with dialup. 4chan API caches requests for about 30 seconds, and there's no real way of knowing if the data received is even realtime. I pretty much had to make a rough assumption that given 1 posts-per-second (pps) 10 new posts will be added between the time when displaying the percentage and submitting a new post. It would be easier to just wget scan /v/ instead of fetching the catalog since it updates much faster.  
+
+**Since 4chan API caches requests for about 30 seconds, you must at least wait that set amount of time to get (semi?)accurate information.**
+
 ## Project setup
 In root:
 ```Shell
---
+++ src/components/ChanWatcher.vue
@@ -0,0 +1,166 @@
+<template>
+  <div>
+    <h2>THREAD COUNTER</h2>
+    <div class="rotate-cont">
+      <div v-show="requestPending" class="rotate-arrows">↻</div>
+    </div>
+    <p
+      class="animated"
+      :class="{ 'animate-post': animate }"
+      @animationend="animate = false"
+    >
+      CURRENT LAST ID: <b>{{ lastPostID }}</b>
+    </p>
+    <p>
+      ID GREW BY <b>{{ delta }}</b>
+    </p>
+    <p>
+      CURRENT POSTS-PER-SECOND: <b>{{ postsPerSecond }}</b>
+    </p>
+    <p>
+      TIME IT TOOK TO UPDATE: <b>{{ timeBetweenUpdates }} SECONDS</b>
+    </p>
+    <p>
+      TIME PASSED SINCE LAST UPDATE: <b>{{ timePassedSeconds }}</b>
+    </p>
+    <p>
+      CHANCE OF FIVE: <b>{{ chanceOf5 }}%</b>
+    </p>
+    <label for="toggle">Toggle Interval</label>
+    <input name="toggle" type="checkbox" @change="toggleInterval()" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "chan-watcher",
+  data: function() {
+    return {
+      lastPostID: 0,
+      catalog: [],
+      lastPost: [],
+      intervalID: null,
+      requestPending: false,
+      animate: false,
+      delta: 0,
+      timeStamp: null,
+      postsPerSecond: null,
+      timeBetweenUpdates: 0,
+      timePassedSeconds: 0
+    };
+  },
+  created() {
+    this.initRefresh();
+  },
+  computed: {
+    chanceOf5() {
+      /*
+      based on pure observation, 
+      i estimate that given 1 post per second (pps), around 10 new posts will be added between an update and clicking on submit as quickly as possible, 
+      and 20 new posts per 2pps
+      and i calculate this chance based on that assumption
+      */
+      return (
+        100 -
+        Math.abs(((this.postsPerSecond * 10 + this.lastPostID) % 10) - 5) * 20
+      ).toFixed(2);
+    }
+  },
+  methods: {
+    initRefresh() {
+      this.refreshStart();
+      this.intervalID = setInterval(this.refreshStart, 1000);
+    },
+    async refreshStart() {
+      if (!this.requestPending) {
+        this.requestPending = true;
+        await this.refreshPostID();
+        this.requestPending = false;
+      }
+    },
+    async refreshPostID() {
+      this.catalog = await this.$api.getCatalog("v");
+      this.analyzeCatalog();
+    },
+    toggleInterval() {
+      if (this.intervalID) {
+        clearInterval(this.intervalID);
+        this.intervalID = null;
+      } else {
+        this.initRefresh();
+      }
+    },
+    analyzeCatalog() {
+      let newMax = this.catalog.reduce((finalMaxID, page) => {
+        let pageMaxID = page.threads.reduce((maxID, thread) => {
+          let lastPost = this.getLastPostIDFromThread(thread);
+          return lastPost > maxID ? lastPost : maxID;
+        }, 0);
+        return pageMaxID > finalMaxID ? pageMaxID : finalMaxID;
+      }, 0);
+
+      this.animate = newMax - this.lastPostID;
+      this.timePassedSeconds = (Date.now() - this.timeStamp) / 1000;
+      if (this.animate) {
+        this.timeBetweenUpdates = this.timePassedSeconds;
+        this.delta = newMax - this.lastPostID;
+        this.postsPerSecond = (this.delta / this.timePassedSeconds).toFixed(2);
+        this.timeStamp = Date.now();
+      }
+      this.lastPostID = newMax;
+    },
+    getLastPostIDFromThread(thread) {
+      if (!thread.last_replies) {
+        return thread.no;
+      }
+      return thread.last_replies[thread.last_replies.length - 1].no;
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+
+.animate-post {
+  animation: highlight 2s;
+}
+.rotate-cont {
+  height: 50px;
+  overflow: hidden;
+}
+
+.rotate-arrows {
+  display: block;
+  margin: auto;
+  padding: 0;
+  color: coral;
+  border: 0;
+  animation: rotate 0.5s linear infinite;
+}
+
+@keyframes rotate {
+  0% {
+    transform: rotate(0deg);
+  }
+  50% {
+    transform: rotate(180deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes highlight {
+  50% {
+    color: crimson;
+    filter: drop-shadow(0px 0px 4px black);
+  }
+  100% {
+    color: black;
+    filter: drop-shadow(0px 0px 0px black);
+  }
+}
+</style>
